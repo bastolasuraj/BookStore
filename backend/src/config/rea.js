c601ad070b81aa3eb4104aba77fa0a206bb87705
Suraj Bastola <bastolasuraj@hotmail.com>
2020-07-06 23:37:49 +0545

    Backend Users and Books Completed

---
+++ backend/src/config/rea.js
@@ -0,0 +1,9 @@
+module.exports = {
+    APP_PORT: 5000,
+    API_KEYS: "iufcvbnmnbgyujksdiuvygnwd",
+    DB_URL:"mongodb+srv://bastolasuraj:Ynirwiv6mk@clustersb.qkwmm.mongodb.net/BookStore?retryWrites=true&w=majority",
+    ACCESS_TOKEN: "YNIRWIV6MK",
+    REFRESH_TOKEN: "YY2N4I4R9W",
+    ACCESS_EXPIRY:"1d",
+    REFRESH_EXPIRY:"7d",
+}
